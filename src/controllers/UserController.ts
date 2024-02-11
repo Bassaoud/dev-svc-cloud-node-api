83a810302b3a2b0c894a9b57d2200d68461e9b1b
Bassaoud <mohamed.bassaoud@yahoo.fr>
2024-02-11 21:23:11 +0100

    commit fix deprection

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,10 @@
 // src/controllers/UserController.ts
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../entities/User';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  const userRepository = getRepository(User);
+  const userRepository = getCustomRepository(UserRepository);
   const users = await userRepository.find();
   res.json(users);
 };
